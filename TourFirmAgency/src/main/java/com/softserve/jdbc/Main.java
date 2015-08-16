cb686b888434672575af3f6f54dff6f129886f4a
luckyvch <mobilede449@gmail.com>
2015-08-16 15:36:15 +0300

    change method i main class

--- TourFirmAgency/src/main/java/com/softserve/jdbc/Main.java
+++ TourFirmAgency/src/main/java/com/softserve/jdbc/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		DataBaseController controller = new DataBaseController();
 		controller.GetConnection();
-		controller.findVisasInThePeriodOfTime();	
+		controller.findVisa();	
 		controller.CloseConnection();
 	}
 
