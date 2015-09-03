deafc4fa2d3c0e766ed5a43a541565e10043732c
Kononchuk-B <danon.konon@gmail.com>
2015-09-03 20:15:17 +0300

    added booking form

--- WebTourfirm/src/main/java/com/softserve/controller/RoomController.java
+++ WebTourfirm/src/main/java/com/softserve/controller/RoomController.java
@@ -1,5 +1,16 @@
 package com.softserve.controller;
 
+import com.softserve.sevice.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/rooms")
 public class RoomController {
+
+    @Autowired
+    private RoomService roomtService;
+
 
 }
